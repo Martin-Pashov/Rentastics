0130ac10d6c8db3097f0ef3bc7c7bd2feb28266e
Martin <martins124124@gmail.com>
2024-03-01 01:39:26 +0200

    created the beginner header component
    created the header component and made it so that it is visible on every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Offers from './pages/Offers'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers'
+import Header from './components/Header';
 
 export default function App() {
   return (
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
