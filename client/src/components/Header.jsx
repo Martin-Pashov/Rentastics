2b5ffe88dd0549656f7fe8f4f8c3c6903e1ae10d
Martin <martins124124@gmail.com>
2024-03-01 02:03:56 +0200

    Added basic styling to the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import logo from '../../public/images/logo.svg';
 
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='bg-white border-b shadow-sm sticky'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <img src={logo} alt='Martin Estate Logo' className='h-8 sm:h-10 cursor-pointer' />
+            <form className='bg-slate-100 p-3 rounded-lg'>
+                <input type='text' placeholder='Search...' className='bg-transparent'/>
+            </form>
+        </div>
+    </header>
   )
 }
