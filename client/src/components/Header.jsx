c3e1f612d2da393643c75958b9ba7f37cd234da8
Martin <martins124124@gmail.com>
2024-03-01 02:25:44 +0200

    search bar has been styled

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import logo from '../../public/images/logo.svg';
+import logo from '../../public/images/logo.svg'
+import { FaSearch } from 'react-icons/fa'
 
 export default function Header() {
   return (
     <header className='bg-white border-b shadow-sm sticky'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <img src={logo} alt='Martin Estate Logo' className='h-8 sm:h-10 cursor-pointer' />
-            <form className='bg-slate-100 p-3 rounded-lg'>
-                <input type='text' placeholder='Search...' className='bg-transparent'/>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 md:w-48' />
+                <FaSearch className='text-black' />
             </form>
         </div>
     </header>
   )
 }
+ 
