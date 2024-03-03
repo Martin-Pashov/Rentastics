a29d3c65b91fa25b8df1db6fc544b5b38384b21f
Martin <martins124124@gmail.com>
2024-03-03 02:25:54 +0200

    test api route enhanced
    trying to figure out how all of this works haha

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,6 +15,4 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get('/test', (req, response) => {
-    response.send('Hello World');
-})
+app.use("/api/user", userRouter);
