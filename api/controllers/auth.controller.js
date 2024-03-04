541e277f316688dd2038454534ec297603aa4445
Martin <martins124124@gmail.com>
2024-03-04 23:24:18 +0200

    started working on the saving user to db functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,8 @@
-export const signup = (req, res) => {
-    console.log(req.body);
+import User from '../models/user.model.js';
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save()
+    res.status(201).json("user created successfully! ");
 };
