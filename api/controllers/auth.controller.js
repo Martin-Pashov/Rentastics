aa9edfd244ec99161143fd232b61e1e8e3657b23
Martin <martins124124@gmail.com>
2024-03-04 23:38:12 +0200

    Enhance user signup error handling.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,18 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    await newUser.save()
-    res.status(201).json("user created successfully! ");
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+
+        await newUser.save();
+
+        res.status(201).json({ message: "User created successfully!" });
+    } 
+    
+    catch (error) {
+        console.error("Error during user signup:", error);
+        res.status(500).json({ error: error.message || "Internal Server Error" });
+    }
 };
