f3a882bf4a5a0268813c40b8330580ab1431f676
Martin <martins124124@gmail.com>
2024-03-06 00:36:07 +0200

    Enhance SignIn server-side logic with detailed error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,3 +44,27 @@ export const signup = async (req, res, next) => {
         next(errorHandler(500, 'An error occurred during user signup.'));
     }
 };
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found.'));
+        }
+
+        const isValidPassword = bcrypt.compareSync(password, validUser.password);
+        if (!isValidPassword) {
+            return next(errorHandler(401, 'Incorrect password.'));
+        }
+
+        res.status(200).json({ success: true, message: 'Sign in successful!', user: validUser});
+    } 
+    
+    catch (error) {
+        // Handle unexpected errors
+        next(errorHandler(500, 'An error occurred during sign-in.'));
+    }
+};
