657ff88bb56c7989d6f50020424de2fc20f5daa4
Martin <martins124124@gmail.com>
2024-03-05 23:23:11 +0200

    Added user signup API call and response logging

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -13,7 +13,16 @@ export default function SignUp() {
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('/api/auth/signup', formData);
+    const res = await fetch('/api/auth/signup', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData), 
+    });
+    const data = await res.json();
+    console.log(data);
   }
 
   console.log(formData);
