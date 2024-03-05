d6d76916520c51cf8f077fb6ae950113d6a4c155
Martin <martins124124@gmail.com>
2024-03-05 11:53:58 +0200

    Update Sign Up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,23 @@
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 '>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
-        <button disabled className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+      <h1 className='text-3xl font-semibold text-center mb-6 text-gray-800'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' className='border rounded-lg p-3 focus:outline-none focus:border-blue-500' id='username'/>
+        <input type="email" placeholder='Email' className='border rounded-lg p-3 focus:outline-none focus:border-blue-500' id='email'/>
+        <input type="password" placeholder='Password' className='border rounded-lg p-3 focus:outline-none focus:border-blue-500' id='password'/>
+        <button disabled className='bg-blue-500 text-white p-3 rounded-lg uppercase cursor-not-allowed opacity-50'>Sign Up</button>
       </form>
 
-      <div className='flex gap-2 mt-5'>
-        <p>Have an account?</p>
-        <Link to={"/sign-in"}> 
-          <span className='text-blue-700'>Sign in</span>
+      <div className='flex items-center mt-6'>
+        <p className='mr-2 text-gray-600'>Have an account?</p>
+        <Link to='/sign-in' className='text-blue-500'>
+          Sign in
         </Link>
       </div>
     </div>
-  )
+  );
 }
