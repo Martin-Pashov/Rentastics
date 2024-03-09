df09a227719e34877ac90e1380c957f14b7da30a
Martin <martins124124@gmail.com>
2024-03-09 23:50:24 +0200

    Implement update user endpoint
    Created the API endpoint for updating user profiles, allowing users to modify their account information. This includes handling user authentication and authorization to ensure that users can only update their own accounts.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,38 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from '../models/user.model.js';
+
 export const test = (request, response) => {
     response.json({
         message: 'api route is working',
     });
+};
+
+
+export const updateUserInfo = async (request, response, next) => {
+    if (request.user.id !== request.params.id) {
+        return next(errorHandler(401, 'Unauthorized: You can only update your own account.'));
+    }
+
+    try {
+        if (request.body.password) {
+            request.body.password = bcryptjs.hashSync(request.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(request.params.id, {
+            $set: {
+                username: request.body.username,
+                email: request.body.email,
+                password: request.body.password,
+                avatar: request.body.avatar,
+            }
+        }, {new: true})
+
+        const {password, ...rest} = updatedUser._doc
+        response.status(200).json({rest})
+    }
+
+    catch (error) {
+        next(error);
+    }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (request, response, next) => {
+    const token = request.cookies.access_token;
+
+    if (!token) {
+        return next(errorHandler(401, 'Unauthorized: User lacks the necessary permissions to access this resource.'));
+    }
+    
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+        if (error) {
+            return next(errorHandler(403, 'Forbidden: Access to the requested resource is not allowed.'));
+        }
+
+        request.user = user;
+        next();
+    });
+};
