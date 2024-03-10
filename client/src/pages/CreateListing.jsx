e3cee6d86b20e9e0a04a672d05b6bd7c7a16bc32
Martin <martins124124@gmail.com>
2024-03-11 00:14:25 +0200

    Integrated Firebase storage to handle image uploads, including progress tracking

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,12 +1,52 @@
+import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage';
 import { useState } from 'react'
+import { app } from '../firebase';
 
 export default function CreateListing() {
     const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+    });
+
+    console.log(formData);
 
     const handleImageSubmit = (e) => {
         if (files.length > 0 && files.length < 7) {
+            const promises = [];
 
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]));
+            }
+
+            Promise.all(promises).then((urls) => {
+                setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) });
+            });
         }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                (error)=>{
+                    reject(error);
+                }, 
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+            )
+        });
     }
 
     return (
