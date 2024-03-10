4d69ff5d4b1448efa3cd4a43e1522e0c04b6f831
Martin <martins124124@gmail.com>
2024-03-10 18:48:07 +0200

    Minor improvements for the Listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,9 @@
+import Listing from "../models/listing.model.js";
+
 export const createListing = async (request, response, next) => {
     try {
         const listing = await Listing.create(request.body);
+        return response.status(201).json(listing);
     }
 
     catch (error) {
