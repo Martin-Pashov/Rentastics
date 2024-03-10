09678b3647601e9fcbef2e7acc2ee53984f69f1e
Martin <martins124124@gmail.com>
2024-03-10 22:54:34 +0200

    Started working on the UI for the Create a Listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Profile from './pages/Profile';
 import Offers from './pages/Offers'
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
 
       <Route element={<PrivateRoute />} >
         <Route path='/profile' element={<Profile />} />
+        <Route path='/create-listing' element={<CreateListing />} />
       </Route>
 
       <Route path='/offers' element={<Offers />} />
---
+++ client/src/pages/CreateListing.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function CreateListing() {
+  return (
+    <main className='p-3 max-w-4xl mx-auto'>
+        <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
+        <form className='flex flex-col sm:flex-row gap-4'>
+            <div className='flex flex-col gap-4 flex-1'>
+                <input type="text" placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='10' required />
+                <input type="text" placeholder='Description' className='border p-3 rounded-lg' id='description' maxLength='5000' minLength='10' required />
+                <input type="text" placeholder='Address' className='border p-3 rounded-lg' id='address' maxLength='1000' minLength='10' required />
+            
+                <div className='flex gap-6 flex-wrap'>
+                    <div className='flex gap-2'>
+                        <input type='checkbox' id='sale' className='w-5'/>
+                        <span className='w-5'>Sell</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <input type='checkbox' id='rent' className='w-5'/>
+                        <span className='w-5'>Rent</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <input type='checkbox' id='parking' className='w-5'/>
+                        <span className='w-5'>Parking spot</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <input type='checkbox' id='furnished' className='w-5'/>
+                        <span className='w-5'>Furnished</span>
+                    </div>
+
+                    <div className='flex gap-2'>
+                        <input type='checkbox' id='offer' className='w-5'/>
+                        <span className='w-5'>Offer</span>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+  )
+}
