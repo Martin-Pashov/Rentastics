98965bab2991be6e0c912ef3f2e823213483eec5
Martin <martins124124@gmail.com>
2024-03-14 00:27:24 +0200

    Added some basic error handling for the Listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 
 export default function Listing() {
     const params = useParams();
+    const Navigate = useNavigate();
     const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
     useEffect(() => {
@@ -20,7 +21,9 @@ export default function Listing() {
                     return;
                 }
 
-                setListing(data)
+                setListing(data);
+                setLoading(false);
+                setLoading(false);
             } 
             
             catch (error) {
@@ -28,12 +31,25 @@ export default function Listing() {
                 setLoading(false);
             }
         }
-    });
+
+        fetchListing();
+    }, [params.listingId]);
 
 
   return (
-    <div>
-      Lis
-    </div>
+    <main>
+        {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+            {error && (
+                <div className="text-center my-7">
+                    <p className='text-2xl'>Oops! Something went wrong.</p>
+                    <p className='text-xl text-gray-500'>Please try again later or return to the homepage.</p>
+                    <button className="flex flex-col items-center justify-center p-3 mt-7 text-gray-700 border border-gray-700 rounded uppercase hover:shadow-lg disabled:opacity-80 w-50% mx-auto" onClick={() => Navigate('/')}>Return to Homepage</button>
+                </div>
+            )}
+
+            {listing && !loading && !error && (
+                <h1>{listing.name}</h1>
+            )}
+    </main>
   )
 }
