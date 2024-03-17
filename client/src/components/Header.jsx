60d2ec81b984e4e14dd9c3385ddfed0924be2ec3
Martin <martins124124@gmail.com>
2024-03-17 21:40:31 +0200

    Started implementing the search functionality with URL query parameters

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,40 @@
 import logo from '../../public/images/logo.svg'
 import { FaSearch } from 'react-icons/fa'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
 
 export default function Header() {
     const {currentUser} = useSelector(state => state.user)
     const location = useLocation();
+    const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState('');
+
     function pathMatchRoute(route) {
         if (route === location.pathname){
             return true;
         }
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+
+        if (searchTermFromUrl) {
+          setSearchTerm(searchTermFromUrl);
+        }
+        
+    }, [location.search]);
 
   return (
     /* 'top-0 z-50' is subject to change due to stylistic reasons. What it does is stick the header to the top of the page so it stays put even if scrolling.*/
@@ -21,8 +45,10 @@ export default function Header() {
             </Link>
 
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 md:w-48' />
-                <FaSearch className='text-black' />
+                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 md:w-48' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                <button>
+                    <FaSearch className='text-slate-600' />
+                </button>
             </form>
             
             <ul className='flex gap-4'>
@@ -46,11 +72,11 @@ export default function Header() {
                 </Link>
 
                 <Link to='/profile'> 
-                    {
-                        currentUser ? (
-                            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="" />
-                        ): <li className={`text-md font-semibold text-gray-400 border-b-[3] border-b-transparent ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`}>Sign In</li>
-                    }
+                    {currentUser ? (
+                        <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="" />
+                    ) : (
+                        <li className={`text-md font-semibold text-gray-400 border-b-[3] border-b-transparent ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`}>Sign In</li>
+                    )}
                 </Link>
             </ul>
         </div>
