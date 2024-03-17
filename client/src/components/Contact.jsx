13cb7a9d1931c6a998d825d6c26957aaa410f333
Martin <martins124124@gmail.com>
2024-03-17 18:55:39 +0200

    Added styling to the message form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 
 export default function Contact({listing}) {
     const [ landlord, setLandlord ] = useState(null);
+    const [message, setMessage] = useState('');
+
+    const onChange = (e) => {
+        setMessage(e.target.value);
+    };
 
     useEffect(() => {
         const fetchLandlord = async () => {
@@ -25,8 +30,9 @@ export default function Contact({listing}) {
   return (
     <>
     {landlord && (
-        <div className=''>
-            <p>Contact <span>{landlord.username}</span></p>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+            <p className='text-lg font-semibold mb-4'>Contact <span className='text-blue-600'>{landlord.username}</span> for <span className='text-green-600 italic'>'{listing.name}'</span></p>
+            <textarea className='w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'name="message" id="message" rows="4" placeholder="Type your message here..." value={message} onChange={onChange}></textarea>
         </div>
     )}
     </>
