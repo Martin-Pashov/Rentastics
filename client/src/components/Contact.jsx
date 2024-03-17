3abb54fa4798e9548fc119c8ec5610733a4ea637
Martin <martins124124@gmail.com>
2024-03-17 18:41:15 +0200

    Started working on the frontend part of the contact component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Contact({listing}) {
+    const [ landlord, setLandlord ] = useState(null);
+
+    useEffect(() => {
+        const fetchLandlord = async () => {
+            try {
+                const response = await fetch(`/api/user/${listing.userRef}`);
+                const data = await response.json();
+
+                setLandlord(data);
+            } 
+            
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchLandlord();
+
+    }, [listing.userRed]);
+
+
   return (
-    <div>
-      Contact
-    </div>
+    <>
+    {landlord && (
+        <div className=''>
+            <p>Contact <span>{landlord.username}</span></p>
+        </div>
+    )}
+    </>
   )
 }
