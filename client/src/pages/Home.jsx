d4d85cf3e22ae4f07aebfee79ef9d40130177eea
Martin <martins124124@gmail.com>
2024-03-19 23:40:34 +0200

    Consolidate API data fetching logic for the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+
+  console.log(saleListings);
+  
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const response = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = await response.json();
+
+        setOfferListings(data);
+        fetchRentListings();
+      } 
+      
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+
+    const fetchRentListings = async () => {
+      try {
+        const response = await fetch('/api/listing/get?type=rent&limit=4');
+        const data = await response.json();
+
+        setRentListings(data);
+        fetchSaleListings();
+      } 
+      
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const data = await res.json();
+        setSaleListings(data);
+      } 
+      
+      catch (error) {
+        log(error);
+      }
+    };
+
+    fetchOfferListings();
+  }, []);
+
   return (
     <div>
       {/* Top */}
@@ -38,10 +90,10 @@ export default function Home() {
 
       {/* swiper */}
 
-      
+
 
 
       {/* listing results for offer, sale and rent */}
     </div>
-  )
+  );
 }
